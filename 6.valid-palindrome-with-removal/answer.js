69bd92ce5ee8ed195db94574bb3657d9b06d02cc
Danillo Pinheiro Neto <danillopneto@outlook.com>
2022-03-01 16:12:07 -0300

    Including Valid Palindrome With Removal

---
+++ 6.valid-palindrome-with-removal/answer.js
@@ -0,0 +1,28 @@
+let isPalindromeWithRemoval = (word) => {
+  let isValidPalindrome = (palindrome) => {    
+    return palindrome == palindrome.split('').reverse().join('');
+  }
+
+  if (isValidPalindrome(word)) return true;
+
+  const possibleWords = word.split('').map((el, i) => word.slice(0, i) + word.slice(i + 1));  
+  for (let w of possibleWords) {
+    if (isValidPalindrome(w)) {
+      return true;
+    }
+  };
+  
+  return false;
+}
+
+let testResult = (answer) => {
+  console.log(answer('abcba')); // true
+  console.log(answer('foobof')); // true
+  console.log(answer('abccab')); // false
+  console.log(answer('teneet')); // true
+  console.log(answer('aiboffobia')); // true
+  console.log(answer('soppapos')); // true
+  console.log(answer('pelpepl')); // false
+}
+
+testResult(isPalindromeWithRemoval);
---
+++ 6.valid-palindrome-with-removal/readme.md
@@ -0,0 +1,12 @@
+# Valid Palindrome with Removal
+
+## This question is asked by `Facebook`.  
+
+Given a string and the ability to delete at most one character, return whether or not it can form a palindrome.  
+Note: a palindrome is a sequence of characters that reads the same forwards and backwards.  
+
+Ex: Given the following strings...  
+
+>`"abcba"`, return `true`  
+`"foobof"`, return `true` (remove the first 'o', the second 'o', or 'b')  
+`"abccab"`, return `false`
